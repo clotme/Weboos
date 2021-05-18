85929278130e156be2a10ca7aaf57088fd6dbb76
aidil-lgr <71768613+aidil-lgr@users.noreply.github.com>
2021-05-18 13:15:47 +0200

    seleccio 18/05
    es comença a programar la selecció de epersonatges (array, però no toggle group)

--- Assets/Scripts/ScrMenuSeleccio.cs
+++ Assets/Scripts/ScrMenuSeleccio.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ScrMenuSeleccio : MonoBehaviour
 {
@@ -11,12 +12,21 @@ public class ScrMenuSeleccio : MonoBehaviour
     ///         Script utilitzat per gestionar la pantalla de menú de selecció.
     /// AUTOR:  Lídia García Romero
     /// DATA:   17/05/2021
-    /// VERSIÓ: 1.0
+    /// VERSIÓ: 2.0
     /// CONTROL DE VERSIONS
     ///         1.0: primera versió on només es programa el so dels botons i el botó back.
+    ///         2.0: Es programa el començament de la selecció de personages, i es recorre
+    ///             l'array correctament. Falta per programar la selecció de personatges
+    ///             amb toggle group.
     /// ----------------------------------------------------------------------------------
     /// </summary>
-    
+
+    //Seleccio de personatges_____________________________________________________________
+    [SerializeField] Image playerEscollit1, playerEscollit2;
+    [SerializeField] Sprite[] personatges;
+    int index1, index2 = 0;
+    //____________________________________________________________________________________
+
     void Start()
     {
         
@@ -25,10 +35,13 @@ public class ScrMenuSeleccio : MonoBehaviour
 
     void Update()
     {
-        
-    }
+        //Seleccio de personatges_____________________________________________________________
+        playerEscollit1.sprite = personatges[index1];
+        playerEscollit2.sprite = personatges[index2];
+        //____________________________________________________________________________________
+}
 
-    public void ReproduirSo(AudioSource so)
+public void ReproduirSo(AudioSource so)
     {
         so.Play();
     }
@@ -36,5 +49,63 @@ public class ScrMenuSeleccio : MonoBehaviour
     public void BackToMenu()
     {
         SceneManager.LoadScene("Inici");
+    }
+
+    public void PlayerArray1(string direccio) //Recorrer la llista de personatges
+    {
+        if (direccio == "dreta")
+        {
+            if (index1 >= 8) //per evitar un index out of range
+            {
+                index1 = 0;
+            }
+
+            else
+            {
+                index1 += 1;
+            }
+        }
+
+        if (direccio == "esquerra")
+        {
+            if (index1 <= 0)
+            {
+                index1 = 8;
+            }
+
+            else
+            {
+                index1 -= 1;
+            }
+        }
+    }
+
+    public void PlayerArray2(string direccio) //Recorrer la llista de personatges
+    {
+        if (direccio == "dreta")
+        {
+            if (index2 >= 8) //per evitar un index out of range
+            {
+                index2 = 0;
+            }
+
+            else
+            {
+                index2 += 1;
+            }
+        }
+
+        if (direccio == "esquerra")
+        {
+            if (index2 <= 0)
+            {
+                index2 = 8;
+            }
+
+            else
+            {
+                index2 -= 1;
+            }
+        }
     }
 }
